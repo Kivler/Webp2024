fa1f0e24fbea244468cc2aad48687d64027aab15
Kivler <113400157+Kivler@users.noreply.github.com>
2024-05-06 14:33:01 +0800

    Update cgu_login.js

--- lab4/react_login/src/cgu_login.js
+++ lab4/react_login/src/cgu_login.js
@@ -13,6 +13,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Lighthouse from './jpg';
+
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
